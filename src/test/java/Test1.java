40a76f213153cc9423941dd39f23b53b525c1659
steja <tejas522@gmail.com>
2025-03-16 00:14:42 +0530

    Latest Code

--- src/test/java/Test1.java
+++ src/test/java/Test1.java
@@ -1,11 +1,18 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 public class Test1 {
 
 
     @Test
-    public void A()
-    {
-
+    public void A() {
+        System.out.println("Demo Test Successfull");
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://www.google.com");
+        System.out.println("Title: " + driver.getTitle());
     }
 }

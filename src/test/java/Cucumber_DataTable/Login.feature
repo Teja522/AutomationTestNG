1af3fb600c45ab995913b46d904995e10e4476e9
steja <tejas522@gmail.com>
2025-03-16 23:50:05 +0530

    Latest

--- src/test/java/Cucumber_DataTable/Login.feature
+++ src/test/java/Cucumber_DataTable/Login.feature
@@ -2,9 +2,5 @@ Feature: Login functionality
 
   Scenario: Verify login with multiple credentials
     Given User is on the login page
-    When User enters the following credentials
-      | username      | password     |
-      | standard_user | secret_sauce |
-      | visual_user   | secret_sauce |
-#      | error_user    | secret_sauce |
+    When User Checks Broken Images
     Then User should be logged in successfully
--- src/test/java/Cucumber_DataTable/LoginSteps.java
+++ src/test/java/Cucumber_DataTable/LoginSteps.java
@@ -10,12 +10,15 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.List;
 import java.util.Map;
 
 public class LoginSteps {
     WebDriver driver;
-    int i = 0;
+
 
     @Given("User is on the login page")
     public void user_is_on_the_login_page() {
@@ -23,38 +26,35 @@ public class LoginSteps {
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
         driver.manage().window().maximize();
-        driver.get("https://ultimateqa.com/filling-out-forms/");
+        driver.get("https://the-internet.herokuapp.com/broken_images");
+        System.out.println("Driver Launched Successfully");
     }
 
-    @When("User enters the following credentials")
-    public void userEntersCredentials(DataTable dataTable) throws InterruptedException {
-        List<Map<String, String>> credentials = dataTable.asMaps(String.class, String.class);
 
+    @When("User Checks Broken Images")
+    public void userChecksBrokenImages() throws IOException {
+        List<WebElement> ImgLinks = driver.findElements(By.tagName("img"));
+        System.out.println("Total Image Count : " + ImgLinks.size());
+        int count = 0;
+        for (WebElement link : ImgLinks) {
+            String Imgurl = link.getAttribute("src");
 
-        for (Map<String, String> data : credentials) {
-            String username = data.get("username");
-            String password = data.get("password");
-            System.out.println("Logging in with: " + username + " / " + password);
+            URL urls = new URL(Imgurl);
+            HttpURLConnection conn = (HttpURLConnection) urls.openConnection();
 
-            WebElement input1 = driver.findElement(By.xpath("//input[@id='et_pb_contact_name_0']"));
-            WebElement input2 = driver.findElement(By.xpath("//textarea[@id='et_pb_contact_message_0']"));
-            WebElement Submit = driver.findElement(By.xpath("//button[@name='et_builder_submit_button']"));
-            input1.sendKeys(username);
-            input2.sendKeys(password);
-            Submit.click();
-            Thread.sleep(5000);
-            driver.navigate().refresh();
-            Thread.sleep(2000);
+            conn.connect();
+            int responseCode = conn.getResponseCode();
+            if (responseCode >= 400) {
+                System.out.println("Image Is Broken " + Imgurl);
+                count++;
+            }
 
         }
     }
 
     @Then("User should be logged in successfully")
     public void user_should_be_logged_in_successfully() {
-        System.out.println("End");
-        //Assert.fail();
+        System.out.println("Driver Closed Successfully");
         driver.quit();
     }
-
-
 }
